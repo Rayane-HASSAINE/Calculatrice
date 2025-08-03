00adf45242204daa400cf11f3f4dc1b35e9559b6
Rayane-HASSAINE <hassaine.ray@gmail.com>
2025-08-04 01:17:33 +0200

    Modification des programmes onClick pour que ce la s'accorde avec les normes React, ajout de onClick sur l'intégralité des bouttons. Ajout du script DE avec un slice (0, -1 afin de supprimer le dernier chiffre integrer a la variable d'état

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
 
@@ -13,36 +12,37 @@ function App() {
         <div className="calculator">
           <form>
             <div className='display'>
-          <input type="text" name="display" value={display} readOnly />
+          <input className="input" type="text" name="display" value={display} readOnly />
             </div>
             <div>
-              <input type="button" value="AC" />
-              <input type="button" value="DE" />
-              <input type="button" value="." />
-              <input type="button" value="/" />
+              <input  type="button" value="AC" className="operator" onClick={() => setDisplay("")}/>
+              <input  type="button" value="DE" className="operator" onClick={() => setDisplay(display.slice(0, -1))} />
+              <input className="operator" type="button" value="." onClick={() => setDisplay(display + ".")}/>
+              <input className="operator" type="button" value="/" onClick={() => setDisplay(display + "/")} />
             </div>
             <div>
               <input type="button" value="7" onClick={() => setDisplay(display + "7")} />
-              <input type="button" value="8" />
-              <input type="button" value="9" />
-              <input type="button" value="*" />
+              <input type="button" value="8" onClick={() => setDisplay(display + "8")}/>
+              <input type="button" value="9" onClick={() => setDisplay(display + "9")}/>
+              <input className="operator" type="button" value="*" onClick={() => setDisplay(display + "*")}/>
             </div>
             <div>
-              <input type="button" value="4" />
-              <input type="button" value="5" />
-              <input type="button" value="6" />
-              <input type="button" value="-" />
+              <input type="button" value="4" onClick={() => setDisplay(display + "4")}/>
+              <input type="button" value="5" onClick={() => setDisplay(display + "5")}/>
+              <input type="button" value="6" onClick={() => setDisplay(display + "6")}/>
+              <input className="operator" type="button" value="-" onClick={() => setDisplay(display + "-")}/>
             </div>
             <div>
-              <input type="button" value="1" />
-              <input type="button" value="2" />
-              <input type="button" value="3" />
-              <input type="button" value="+" />
+              <input type="button" value="1" onClick={() => setDisplay(display + "1")}/>
+              <input type="button" value="2" onClick={() => setDisplay(display + "2")}/>
+              <input type="button" value="3" onClick={() => setDisplay(display + "3")}/>
+              <input className="operator" type="button" value="+" onClick={() => setDisplay(display + "+")}/>
             </div>
             <div>
-              <input type="button" value="00" />
-              <input type="button" value="0" />
-              <input type="button" value="=" class="equal" />
+              <input type="button" value="00" onClick={() => setDisplay(display + "00")}/>
+              <input type="button" value="0" onClick={() => setDisplay(display + "0")}/>
+              <input className="operator" type="button" value="=" class="equal" onClick={() => eval(display)} />
+              <input type="button" value="=" className="operator" onClick={() => setDisplay(String(eval(display)))}/>
             </div>
           </form>
         </div>
